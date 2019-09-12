4d1c6dd7ef4ecfbbc1b3aea414bc928aa468661e
Pancho Labrador Jr <plabradorjr@gmail.com>
2019-09-12 04:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holiday, ${name}"
+}
+
+funtion happyHolidayTo(holiday, name) {
+  return "happy ${holiday}, ${name}"
+}
