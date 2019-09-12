08a13c1c03abdb5b75e16b894358485c01671148
Pancho Labrador Jr <plabradorjr@gmail.com>
2019-09-12 04:19:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 function happyHolidays() {
-  return "Happy holidays!"
+  return `Happy holidays!
 }
 
 function happyHolidaysTo(name) {
@@ -13,5 +13,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
+  return "It's ${days} days until ${holiday}!"
 }
